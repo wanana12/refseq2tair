96752b4fd98989bbae243067f3d1d909ca7608ec
wanana12 <134196596+wanana12@users.noreply.github.com>
2023-10-24 02:56:24 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# refseq2tair
+refseqIDをtairIDに変換
+
+https://bi.biopapyrus.jp/rnaseq/annotation/org.at.tair.db.html
---
+++ refseq2tair.R
@@ -0,0 +1,17 @@
+# refseqIDをtairIDに変換
+setwd("~/refseq2tair")
+getwd()
+
+library(org.At.tair.db)
+
+table = read.table("input.txt", header = TRUE, sep="\t")
+library(stringr)
+table$refseq <- str_sub(table$RefSeq, end = -3)
+
+ID <- as.list(org.At.tairREFSEQ2TAIR)
+table$tair <- ID[table$refseq]
+sapply(table, class)
+table <- dplyr::mutate(table, tair = as.character(tair)) # list型をcharacter型に変換
+sapply(table, class)
+
+write.table(table, file = "output.txt", col.names = T, row.names = F, sep="\t", quote = F)
